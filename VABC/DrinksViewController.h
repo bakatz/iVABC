3dd4d6e16efbc7e9fa5d6037d1467aba273c4d96
bakatz <bakatz91@gmail.com>
2013-11-11 19:41:20 -0500

    Added controller stubs for drinks and locations.

---
+++ VABC/DrinksViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrinksViewController.h
+//  VABC
+//
+//  Created by Benjamin Katz on 11/11/13.
+//  Copyright (c) 2013 Benjamin Katz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinksViewController : UIViewController
+
+@end
---
+++ VABC/DrinksViewController.m
@@ -0,0 +1,38 @@
+//
+//  DrinksViewController.m
+//  VABC
+//
+//  Created by Benjamin Katz on 11/11/13.
+//  Copyright (c) 2013 Benjamin Katz. All rights reserved.
+//
+
+#import "DrinksViewController.h"
+
+@interface DrinksViewController ()
+
+@end
+
+@implementation DrinksViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
